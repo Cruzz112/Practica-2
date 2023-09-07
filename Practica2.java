e11f706a874ae2bbeea7a2f4ab77949977a4b785
Cruzz112 <143724511+Cruzz112@users.noreply.github.com>
2023-09-07 09:52:53 -0500

    Add files via upload

---
+++ Practica2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica2;
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int Base = 70;
+       int altura = 5;
+       int areaRectangulo = Base*altura;
+       
+ System.out.println("El area del Rectangulo es:"+areaRectangulo);
+  String nombre = "Alfredo Cruz Gonzalez";
+        System.out.println("El nombre es"+nombre);
+  
+ 
+       
+   ;
+    }
+    
+}
